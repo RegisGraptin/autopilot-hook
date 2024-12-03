223ba551191dc46058f30162a8174e5271e58ddd
rere <regis-graptin@sfr.fr>
2024-12-03 10:50:48 +0700

    feat: update readme in model

---
+++ model/README.md
@@ -0,0 +1,54 @@
+
+# ForcastML - Market Volatility Forecast
+
+Our goal is to develop a simple machine learning model to predict **market volatility** based on historical price behavior.
+This model will initially serve as a proof of concept for forecasting price volatility and will later be integrated into a smart contract using Stylus from Arbitrum to enable on-chain execution.
+By leveraging historical data, we aim to build a robust system that dynamically adjusts fees based on market conditions, helping to protect liquidity providers (LPs) from the risks associated with sudden price volatility in the cryptocurrency market.
+
+## Data Collection & Scope
+
+Our research focuses on the **ETH/USD price data** from **November 2023** to **November 2024**, sampled at **15-minute intervals**.
+This dataset provides a granular view of price fluctuations, which will serve as the foundation for our volatility prediction model.
+
+### Limitations & Considerations:
+
+- We are using only one year of data, which introduces a potential bias, as cryptocurrency markets often follow longer cycles (e.g., 4-year cycles).
+- The crypto market is inherently more volatile compared to traditional financial markets, which may impact the accuracy and stability of our model.
+- As the cryptocurrency market matures, past volatility patterns may become less relevant. However, analyzing historical events like flash crashes can still provide valuable insights and help validate our approach in real-world scenarios.
+
+
+## Model Design & Implementation
+
+Our approach uses historical price data to predict price volatility. For that, we are reling on the last **20 data points** (15-minute intervals) to capture short-term market behavior.
+
+### Data Transformation:
+
+We start by converting the raw price data into **tick values** and calculate the **tick differences** between each 15-minute interval to measure volatility. To measure volatility, we are going to focus our approach on price variation over a window of 20 intervals.
+
+- For each time step t, we group the previous 20 tick variations (t-20 to t) to compute the standard deviation.
+- The standard deviation serves as our primary indicator of price variation, which we aim to predict.
+
+Given the need for on-chain deployment, our initial implementation uses a Linear Regression model as a proof of concept due to its simplicity and efficiency.
+To avoid handling floating-point numbers on-chain, we decided to upscale both the input data and the model coefficients to be able to use int256 directly in the smart contract. While this introduces minor approximations, it ensures compatibility with on-chain arithmetic without compromising too much prediction accuracy.
+
+## Environement
+
+Some command to create locally your environment:
+
+```bash
+uv venv
+source .venv/bin/activate
+```
+
+
+Manage new dependencies
+```bash
+uv pip freeze | uv pip compile - -o requirements.txt
+```
+
+## Run script
+
+```bash
+source .venv/bin/activate
+python main.py
+```
--- model/main.py
+++ model/main.py
@@ -10,7 +10,12 @@ from sklearn.linear_model import LinearRegression
 
 
 def price_to_tick(price):
+    """Convert price to tick."""    
     return np.log(price) / np.log(1.0001)
+
+def convert_date(df):
+    df['date'] = pd.to_datetime(df['date'].astype(str), format='%Y%m%d')
+    df['datetime'] = pd.to_datetime(df['date'].astype(str) + ' ' + df['time'])
 
 def plot_data(df, column="tick_variation"):
     plt.figure(figsize=(12, 6))
@@ -22,23 +27,25 @@ def plot_data(df, column="tick_variation"):
     plt.legend()
     plt.show()
 
-
-def convert_date(df):
-    df['date'] = pd.to_datetime(df['date'].astype(str), format='%Y%m%d')
-    df['datetime'] = pd.to_datetime(df['date'].astype(str) + ' ' + df['time'])
+def plot_boxplot(df):
+    plt.figure(figsize=(12, 6))
+    sns.boxplot(df["abs_tick_variation"], orient="y")
+    plt.legend()
+    plt.show()
     
 
 def prepare_data_set():
     df = pd.read_csv("./data/ETHUSD_15_2023-11-29_2024-11-28.csv")
 
+    # Convert price to tick
     df["tick"] = df["close"].apply(price_to_tick)
     df["tick"] = df["tick"].round().astype(int)
 
+    # Compute tick variation between t and t-1
     df["tick_variation"] = np.log(df["tick"] / df["tick"].shift(1))
 
     # Convert date and time to datetime
     convert_date(df)
-    # plot_data(df)
 
     # Drop first data as no tick variation data
     df = df.drop(index=0)
@@ -73,6 +80,7 @@ def create_training_data(df, column="rolling_std"):
 
 
 def parse_model_to_stylus(model):
+    """Print model to stylus format."""    
 
     fn_data = """
     fn compute_forcast_volatility(
@@ -88,16 +96,12 @@ def parse_model_to_stylus(model):
 
 
     fn_data += "\t];\n"
+    fn_data += "\t\n"
 
+    fn_data += "\t // FIXME:"
 
     print(fn_data)
 
-        # 
-
-    
-
-        # Ok(new_volatility)
-    # }
 
 
 
@@ -109,7 +113,6 @@ if __name__ == "__main__":
 
     # Scaled due to integer constraint in smart contract
     df['tick_variation'] = df['tick_variation'] * 10_000_000
-
     print(df.head())
 
     # Compute the absolute value of the tick variation to focus on volatility (magnitude)
@@ -124,30 +127,15 @@ if __name__ == "__main__":
     # Drop rows where rolling calculations could not be made (first 'window_size' rows)
     df.dropna(inplace=True)
 
-
-    print(df.head())
-
     # Create a target variable: predict the next standard deviation
     df['target'] = df['abs_tick_variation'].shift(-1)  # This shifts the 'tick_variation' column by 1 row (predict next)
 
-
-    # print(df['target'])
-    # print(df['rolling_std'])
-
+    # plot_boxplot(df)
 
     X, y = create_training_data(df)
 
     # Split the data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # print(df['rolling_std'].iloc[0:500])
-
-    # plt.figure(figsize=(12, 6))
-    # sns.lineplot(df['rolling_std'].iloc[0:500])
-    # plt.grid(True)
-    # plt.legend()
-    # plt.show()
-
 
     # Train a simple Linear Regression model
     model = LinearRegression()
@@ -156,9 +144,13 @@ if __name__ == "__main__":
     # Evaluate the model
     print("Model R^2 score on test data:", model.score(X_test, y_test))
 
-    print(model.get_params())
-    print(model.coef_)
-
-
     parse_model_to_stylus(model)    
 
+    # Do some prediction
+    y_normal = model.predict([X_train[20000]])
+    print(y_normal)
+
+    model.coef_ = model.coef_ * 10_000_000
+    y_mod = model.predict([X_train[20000]])
+    print(y_mod / 10_000_000)
+    
